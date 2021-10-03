3059bb6c23d0bebe7c13061b7c10c3c738194aab
Tushar Muralidharan <u7253634@anu.edu.au>
2021-10-03 11:56:30 +1100

    Style website

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # chess-memory
-A visual memory exercise in Chess puzzles
+A visual memory trainer that uses Chess puzzles. Inspired by Human Benchmark's [Visual Memory Test](https://humanbenchmark.com/tests/memory), ChessMemory trains both visual memory and chess tactics at the same time. Check out the site [here](https://tusharmurali.github.io/chess-memory/). 
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-let board = null;
-const game = new Chess();
-const $status = $('#status');
-const $fen = $('#fen');
-const $pgn = $('#pgn');
+let board = null
+let $board = $('#myBoard')
+let game = new Chess()
+let puzzle  = null
+let moves  = null
+let orientation = null
+let counter = undefined
 
-function onDragStart (source, piece, position, orientation) {
+const squareClass = 'square-55d63'
+
+const $memo = $('#memo')
+const $giveUp = $('#giveUp')
+const $next = $('#next')
+const $status = $('#status')
+const $ratingLabel = $('#ratingLabel')
+const $rating = $('#rating')
+const $fenLabel = $('#fenLabel')
+const $fen = $('#fen')
+const $pgn = $('#pgn')
+
+function onDragStart (source, piece) {
     // do not pick up pieces if the game is over
     if (game.game_over()) return false
 
@@ -16,6 +30,8 @@ function onDragStart (source, piece, position, orientation) {
 }
 
 function onDrop (source, target) {
+    const position = game.fen()
+
     // see if the move is legal
     const move = game.move({
         from: source,
@@ -26,13 +42,65 @@ function onDrop (source, target) {
     // illegal move
     if (move === null) return 'snapback'
 
+    // incorrect move
+    if (source + target !== moves[counter]) {
+        // display position after incorrect move
+        board = Chessboard('myBoard', {
+            ...config,
+            orientation,
+            draggable: false,
+            position: game.fen()
+        })
+
+        // revert to position before incorrect move
+        board.position(position)
+        game.load(position)
+
+        showSolution()
+
+        $giveUp.hide()
+        $ratingLabel.show()
+        $rating.show()
+        $fenLabel.show()
+        $fen.show()
+        $next.show()
+
+        return 'snapback'
+    }
+
     updateStatus()
 }
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
 function onSnapEnd () {
+    if (++counter === moves.length) { // puzzle finished
+        board = Chessboard('myBoard', {
+            ...config,
+            orientation,
+            draggable: false,
+            position: game.fen()
+        })
+        $giveUp.hide()
+        $ratingLabel.show()
+        $rating.show()
+        $fenLabel.show()
+        $fen.show()
+        $next.show()
+        return
+    }
+
+    // we make the next move in the puzzle
+    const move = game.move(moves[counter++], { sloppy : true })
+
+    // highlight opponent's move
+    $board.find('.' + squareClass).removeClass('highlight-black')
+    $board.find('.square-' + move.from).addClass('highlight-black')
+    $board.find('.square-' + move.to).addClass('highlight-black')
+
     board.position(game.fen())
+
+    updateStatus()
 }
 
 function updateStatus () {
@@ -64,36 +132,111 @@ function updateStatus () {
     }
 
     $status.html(status)
-    $fen.html(game.fen())
-    $pgn.html(game.pgn())
+    $pgn.html(game.pgn().split("\n").splice(3))
+}
+
+function getPuzzle() {
+    $.get('lichess_db_puzzle.csv', csv => {
+        const data = csv.split("\n")
+
+        // get random puzzle
+        puzzle = data[Math.floor(Math.random() * data.length)].split(",")
+
+        game.load(puzzle[1])
+        moves = puzzle[2].split(" ")
+        orientation = game.turn() === 'b' ? 'white' : 'black'
+        $rating.html(puzzle[3])
+
+        board = Chessboard('myBoard', {
+            ...config,
+            orientation,
+            draggable: false,
+            position: puzzle[1]
+        })
+
+        // make first move of the puzzle
+        const move = game.move(moves[0], { sloppy: true })
+        counter = 1
+
+        // highlight opponent's move
+        $board.find('.' + squareClass).removeClass('highlight-black')
+        $board.find('.square-' + move.from).addClass('highlight-black')
+        $board.find('.square-' + move.to).addClass('highlight-black')
+
+        board.position(game.fen())
+        $fen.html(game.fen())
+
+        updateStatus()
+    })
+
+    // queue remove pieces after memorization time
+    setTimeout(() => {
+        board = Chessboard('myBoard', {
+            ...config,
+            orientation,
+            draggable: true,
+            position: game.fen(),
+            pieceTheme: 'img/chesspieces/blindfold.png'
+        })
+        $giveUp.show()
+    }, 1000 * $memo.val())
+}
+
+function showSolution() {
+    for (let i = counter; i < moves.length; i++) {
+        setTimeout(() => {
+            game.move(moves[i], { sloppy : true })
+            board.position(game.fen())
+            updateStatus()
+        }, (i - counter + 1) * 1000)
+    }
 }
 
 const config = {
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd
-};
+    onDragStart,
+    onDrop,
+    onSnapEnd
+}
 
-board = Chessboard('myBoard', {
-    ...config,
-    draggable: false,
-    position: 'start'
+const memo = localStorage.getItem("memo")
+if (memo)
+    $memo.val(memo)
+else
+    $memo.val(3)
+$memo.on("input", () => {
+    localStorage.setItem("memo", $memo.val())
 })
 
-updateStatus()
-
-setTimeout(() => {
+$giveUp.hide()
+$giveUp.click(() => {
     board = Chessboard('myBoard', {
         ...config,
-        draggable: true,
-        position: game.fen(),
-        pieceTheme: ''
+        orientation,
+        draggable: false,
+        position: game.fen()
     })
-}, 3000)
-
-const puzzles = 'lichess_db_puzzle.csv';
-$.get(puzzles, csv => {
-    const data = $.csv.toArrays(csv)
-    const puzzle = data[Math.floor(Math.random() * data.length)]
-    console.log(puzzle)
+    showSolution()
+    $ratingLabel.show()
+    $rating.show()
+    $fenLabel.show()
+    $fen.show()
+    $giveUp.hide()
+    $next.show()
 })
+
+$next.hide()
+$next.click(() => {
+    getPuzzle()
+    $ratingLabel.hide()
+    $rating.hide()
+    $fenLabel.hide()
+    $fen.hide()
+    $next.hide()
+})
+
+$fenLabel.hide()
+$fen.hide()
+$ratingLabel.hide()
+$rating.hide()
+
+getPuzzle()
